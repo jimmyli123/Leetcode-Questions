727639f9cd168e5c9db2dcb27b9db7027a8b4ed4
Jimmy Li <lijjimmy@gmail.com>
2023-02-03 23:21:14 -0500

    made adjustment to 118

--- 118.js
+++ 118.js
@@ -15,3 +15,19 @@ var generate = function(numRows) {
     }
     return answerArray
 };
+
+// A cleaner answer
+var generate = function(numRows) {
+    if (numRows ===1 ) { return [[1]]}
+    let answer = [[1]]
+    for (let i =1; i<numRows; i++) {
+        let prevRow = answer[i-1]
+        let curRow = [1]
+        for (let j=1; j<i; j++) {
+            curRow[j] = prevRow[j-1] + prevRow[j]
+        }
+        curRow.push(1)
+        answer.push(curRow)
+    }
+    return answer
+};
