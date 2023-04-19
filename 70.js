b6cd64e00869026733dbb72fb52229b787b0a51c
Jimmy Li <lijjimmy@gmail.com>
2023-04-18 23:57:13 -0400

    added small change to #70.

--- 70.js
+++ 70.js
@@ -15,3 +15,13 @@ var climbStairs = function(n) {
     }
     return memoi[n]
 };
+
+// New if condition to check if memoir function has n already.
+var climbStairs = function(n) {
+    let memoir = [1, 1, 2, 3]
+    if (memoir[n]) { return memoir[n]}
+    for (let i = 4; i<=n; i++) {
+        memoir[i] = memoir[i-1] + memoir[i-2]
+    }
+    return memoir[n]
+};
