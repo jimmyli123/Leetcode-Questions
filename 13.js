469915fbae2aad0db95cf6342f18ed940133373a
Jimmy <lijjimmy@gmail.com>
2024-02-27 23:06:25 -0500

    remove unneccessary test in problem 13 and clean it up a little

--- 13.js
+++ 13.js
@@ -1,5 +1,5 @@
 // 13. Roman to Integer
-
+// Test if input had 1 char
 var romanToInt = function(s) {
     let roman = {
         'I':1,
@@ -22,4 +22,29 @@ var romanToInt = function(s) {
         }
     }
     return sum
+};
+
+// Get rid of checking if input was only 1 char. Not needed.
+var romanToInt = function(s) {
+    let roman = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let sum = 0;
+    for (let i=0; i<s.length; i++) {
+        if (roman[s[i]] < roman[s[i+1]]) {
+            sum += roman[s[i+1]] - roman[s[i]];
+            i++;
+        }
+        else {
+            sum += roman[s[i]];
+        }
+    }
+    return sum;
+
 };
